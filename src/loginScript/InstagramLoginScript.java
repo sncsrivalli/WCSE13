d9480789af07b416037a57714230e2f18afb4015
srivalli <snc.srivalli1@gmail.com>
2022-10-17 21:04:16 +0530

    17/10/2022 commit

---
+++ src/loginScript/InstagramLoginScript.java
@@ -0,0 +1,30 @@
+package loginScript;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InstagramLoginScript {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.instagram.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String loginPageTitle = driver.getTitle();
+		if(loginPageTitle.equals("Instagram"))
+			System.out.println("Pass : Login page displayed");
+		else
+			System.out.println("Fail : Login page is not displayed");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).sendKeys("manager");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.quit();
+		
+
+	}
+
+}
