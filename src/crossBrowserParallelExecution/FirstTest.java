3473df0013d29533ae2a1119c2e083c5e122a632
sncsrivalli <109685225+sncsrivalli@users.noreply.github.com>
2023-03-26 16:10:41 +0530

    Update FirstTest.java
    I have added the comment saying 'cross browser parallel'

--- src/crossBrowserParallelExecution/FirstTest.java
+++ src/crossBrowserParallelExecution/FirstTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
+//cross browser parallel
 public class FirstTest {
 	public WebDriver driver;
 	@Parameters("Browser")
